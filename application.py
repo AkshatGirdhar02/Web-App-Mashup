76d71f10217e9072131e814541aa459b34b6f025
AkshatGirdhar02 <73811679+AkshatGirdhar02@users.noreply.github.com>
2023-08-20 23:52:21 +0530

    Update application.py

--- application.py
+++ application.py
@@ -111,13 +111,13 @@ def main():
                     st.write("Mp4 list: ",mp4_filenames_list)
                     for filename in mp4_filenames_list:
                         video=mp.VideoFileClip(filename)
-                    audio=video.audio
-                    if(audio is not None):
-                        mp3_file_name=filename.replace('.mp4','.mp3')
-                        audio.write_audiofile(mp3_file_name)
-                        video.close()
-                    else:
-                        st.write("Audio not available")
+                        audio=video.audio
+                        if(audio is not None):
+                            mp3_file_name=filename.replace('.mp4','.mp3')
+                            audio.write_audiofile(mp3_file_name)
+                            video.close()
+                        else:
+                            st.write("Audio not available")
                     st.write("All videos are converted to audio")
                     mp3_filename_list=glob.glob(os.path.join(pathdir,"*mp3"))
                     st.write("Mp3 file list :",mp3_filename_list)
